37db419de46efde218120e16f861f59acb9f15bd
WilliamWJD <william.jose.wjd@gmail.com>
2024-09-09 14:10:21 -0300

    feat: criando cookie para user

--- src/pages/api/users/index.api.ts
+++ src/pages/api/users/index.api.ts
@@ -1,5 +1,6 @@
 import { prisma } from '@/lib/prisma'
 import { NextApiRequest, NextApiResponse } from 'next'
+import { setCookie } from 'nookies'
 
 type Data = {
   name: string
@@ -15,11 +16,27 @@ export default async function handler(
 
   const { name, username } = req.body
 
+  const userExists = await prisma.user.findUnique({
+    where: {
+      username: req.body.username,
+    },
+  })
+
+  if (userExists) {
+    return res.status(400).json({ message: 'Username already exists' })
+  }
+
   const user = await prisma.user.create({
     data: {
       name,
       username,
     },
+  })
+
+  // criando o cookie
+  setCookie({ res }, '@ignitecall:userId', user.id, {
+    maxAge: 60 * 60 * 24 * 7, // 7days // quanto tempo o cookie vai ficar disponível
+    path: '/',
   })
 
   return res.status(201).json(user)
